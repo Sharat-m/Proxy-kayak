44cd068333b83f35e51fd8d29e45cbc0045431b0
sharat <sharat@farefirst.com>
2024-06-06 19:31:37 +0530

    added the filtered initail code

--- functions/src/core/routes.js
+++ functions/src/core/routes.js
@@ -5,9 +5,11 @@ const router = express.Router();
 const getRouter = require("../api/urlapi");
 const autoRouter = require("../api/autosuggest");
 const dateLessRouter = require("../api/dateLess");
+const filterRouter = require("../api/filterApi");
 
 router.use("/", getRouter);
 router.use("/", autoRouter);
 router.use("/", dateLessRouter);
+router.use("/", filterRouter);
 
 module.exports = router;
